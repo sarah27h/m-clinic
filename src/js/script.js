f807eb1ea02be64d24c935282be13c069bc7e760
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2021-02-04 02:32:08 +0200

    Feat: add functionality for benefits tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,25 @@
-const user = {
-  name: 'test',
-};
+const benefitsTabs = document.querySelector('.benefits__tabs');
+const benefitsTab = document.getElementsByClassName('js-benefits__tab');
+const benefitsService = document.getElementsByClassName('benefits__service');
+
+console.log(benefitsTab);
+console.log(benefitsService);
+
+benefitsTabs.addEventListener('click', (e) => {
+  if (e.target.nodeName.toLowerCase() === 'span') {
+    const clickedTab = e.target;
+    Array.from(benefitsTab).forEach((benefitTab) => {
+      benefitTab.classList.remove('active');
+    });
+    clickedTab.classList.add('active');
+
+    Array.from(benefitsService).forEach((benefitService) => {
+      benefitService.classList.remove('show');
+      benefitService.classList.add('hide');
+      if (benefitService.getAttribute('id') === clickedTab.getAttribute('data-filter')) {
+        benefitService.classList.remove('hide');
+        benefitService.classList.add('show');
+      }
+    });
+  }
+});
