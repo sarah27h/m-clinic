63da02a6d80b5d7ce8e5736e078d874da97dd5b1
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2021-03-08 01:25:08 +0200

    Feat: add testmonials

---
+++ src/js/testmonialsSlider.js
@@ -0,0 +1,25 @@
+import { tns } from '../../node_modules/tiny-slider/src/tiny-slider';
+
+const slider = tns({
+  container: '.testmonials__slider',
+  items: 3,
+  slideBy: 'page',
+  autoplay: true,
+  responsive: {
+    350: {
+      items: 1,
+      nav: false,
+    },
+    500: {
+      items: 2,
+      gutter: 30,
+      nav: true,
+    },
+    900: {
+      items: 3,
+    },
+  },
+  controls: false,
+  navPosition: 'bottom',
+  mouseDrag: true,
+});
